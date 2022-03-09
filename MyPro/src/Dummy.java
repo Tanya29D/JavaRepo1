d0918b56f51821043f830dc05b40511cb97955d0
D669326 <D669326@VW10A26934.principalusa.corp.principal.com>
2022-03-09 13:03:00 +0530

    Java project jdk 1.4 has been created by Mr. A.

---
+++ MyPro/src/Dummy.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Dummy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List mylist=new ArrayList();
+		mylist.add(100);
+		mylist.add("hello");
+		mylist.add(3.4);
+		System.out.println(mylist);
+
+		
+	}
+
+}
+
